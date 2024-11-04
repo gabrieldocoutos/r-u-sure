a9f30f086c6a8e767efc19f2af7d92168bc405fc
Gabriel Do Couto Santos <gabrieldocoutosantos@bob-3.local>
2024-11-03 22:53:25 -0300

    foo

--- content.js
+++ content.js
@@ -5,7 +5,14 @@ const isUrlBlocked = !!blockedHostnames.find(host => {
     return window.location.hostname.includes(host)
 })
 
-if (isUrlBlocked) {     
- window.location.href = `https://gabrieldocoutos.github.io/r-u-sure-website/?callback=${window.location.href}`
+const isUrlAllowed =  !!new URLSearchParams(window.location.search).get("rusure")
+
+if (isUrlBlocked) { 
+
+    if (isUrlAllowed) {
+        console.log("URL is allowed")
+    } else {
+        window.location.href = `https://gabrieldocoutos.github.io/r-u-sure-website/?callback=${window.location.href}`
+    }
     
 }
